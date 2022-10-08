712f78cc6f6c1299e359fa391123e290816bc3ce
angie hernandez <avhernandez16@ucatolica.edu.co>
2022-10-07 20:55:25 -0500

    "spring boot proyecto magnetic media database"

---
+++ magneticmedia/src/main/java/magneticmedia/magneticmedia/MagneticmediaApplication.java
@@ -0,0 +1,13 @@
+package magneticmedia.magneticmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagneticmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagneticmediaApplication.class, args);
+	}
+
+}
---
+++ magneticmedia/src/test/java/magneticmedia/magneticmedia/MagneticmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package magneticmedia.magneticmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagneticmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
