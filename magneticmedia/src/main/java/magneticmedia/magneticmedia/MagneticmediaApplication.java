af392dd6ff76ffadf9d6c90680a232f7f380c820
sala4 <sala4@FI-LabInf.ucatolica.loc>
2022-11-25 09:09:13 -0500

    se agrega tomacat y .war

--- magneticmedia/src/main/java/magneticmedia/magneticmedia/MagneticmediaApplication.java
+++ magneticmedia/src/main/java/magneticmedia/magneticmedia/MagneticmediaApplication.java
@@ -2,11 +2,18 @@ package magneticmedia.magneticmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MagneticmediaApplication {
+public class MagneticmediaApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MagneticmediaApplication.class, args);
 	}
+	@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+			return builder.sources(MagneticmediaApplication.class);
+		}
 }
+
